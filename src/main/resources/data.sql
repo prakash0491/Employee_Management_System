b14fb17cddc3122de57d2f1b693620580bab4184
PRAKASH <121811396+prakash0491@users.noreply.github.com>
2026-02-16 21:56:16 +0530

    Fix SQL syntax by adding quotes around table name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO user (email, password, role, name) VALUES ('admin@demo.com', 'admin123', 'ADMIN', 'Admin User');
-INSERT INTO user (email, password, role, name) VALUES ('hr@demo.com', 'hr123', 'HR', 'HR Specialist');
+INSERT INTO "user" (email, password, role, name) VALUES ('admin@demo.com', 'admin123', 'ADMIN', 'Admin User');
+INSERT INTO "user" (email, password, role, name) VALUES ('hr@demo.com', 'hr123', 'HR', 'HR Specialist');
